83bc9687a506c886df3eb3e4ba2a703b126a40b2
ilya <ilya@F5-279b86aa.localdomain>
2021-10-22 18:46:54 +0000

    edit fragment_main

--- app/src/main/java/com/example/rk1/MainActivity.kt
+++ app/src/main/java/com/example/rk1/MainActivity.kt
@@ -5,55 +5,17 @@ import android.annotation.SuppressLint
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.rk1.R
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = ListAdapter()
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
-        }
-
-        NetworkService.getJSONApi()
-            ?.getData("bash", 50)
-            ?.enqueue(object : Callback<List<PostModel?>?> {
-            @SuppressLint("NotifyDataSetChanged")
-            override fun onResponse(
-                call: Call<List<PostModel?>?>,
-                response: Response<List<PostModel?>?>
-            ) {
-                response.body()?.forEach { it ->
-                    (viewAdapter as ListAdapter).addData(it)
-                }
-                recyclerView.adapter!!.notifyDataSetChanged()
-            }
-
-            override fun onFailure(call: Call<List<PostModel?>?>, t: Throwable) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "An error occurred during networking $t",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        })
+        val fragmentMain = supportFragmentManager.findFragmentById(R.id.fragment_main) as NavHostFragment
+        val navController = fragmentMain.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
--- app/src/main/java/com/example/rk1/fragment_main.kt
+++ app/src/main/java/com/example/rk1/fragment_main.kt
@@ -1,10 +1,17 @@
 package com.example.rk1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +28,17 @@ class fragment_main : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -34,7 +46,38 @@ class fragment_main : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        viewManager = LinearLayoutManager(view.context)
+        viewAdapter = ListAdapter()
+        recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+        NetworkService.getJSONApi()
+            ?.getData("bash", 50)
+            ?.enqueue(object : Callback<List<PostModel?>?> {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onResponse(
+                    call: Call<List<PostModel?>?>,
+                    response: Response<List<PostModel?>?>
+                ) {
+                    response.body()?.forEach { it ->
+                        (viewAdapter as ListAdapter).addData(it)
+                    }
+                    recyclerView.adapter!!.notifyDataSetChanged()
+                }
+
+                override fun onFailure(call: Call<List<PostModel?>?>, t: Throwable) {
+                    Toast.makeText(
+                        view.context,
+                        "An error occurred during networking $t",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
+        return view
     }
 
     companion object {
